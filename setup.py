a77e65279aa72e518d22336884c9f75d2d74e84a
Matt Bennett <matt@bennett.name>
2019-06-19 14:43:54 +0100

    use my branch

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ from setuptools import setup
 
 
 setup(
-    name='pre_commit_dummy_package',
-    version='0.0.0',
-    install_requires=['isort==4.3.20'],
+    name="pre_commit_dummy_package",
+    version="0.0.0",
+    install_requires=[
+        "isort@git+https://github.com/mattbennett/isort.git@4.3.20-dev#egg=4.3.20-dev"
+    ],
 )
